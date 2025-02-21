2739428499b76f6defbb84e3868887ae8adc66c4
Yuno Kim <unokim1547@gmail.com>
2025-02-22 00:27:19 +0900

    null

--- insta/src/components/Test/Test.jsx
+++ insta/src/components/Test/Test.jsx
@@ -1,51 +1,29 @@
 import { useState, useEffect } from "react";
 
 function Test() {
-  const [id, setId] = useState("")
-  const [password, setPassword] = useState("")
-
-  const idHandler = (evt) => {
-    setId(evt.target.value)
+  // gpt뭐함 ㅋㅋ
+  const fulfilledPromiseGen = () => {
+    return new Promise((res, jej) => {
+      res('resolved')
+    })
   }
+  const likeFetch = new Promise((res, rej) => {
+    console.log('fetch완료');
+    res()
+  }).then(() => {
+    if(true){ // 비번 오류
+      console.log('회원이 아니거나 비밀번호를 확인해보세요');
+      return
+    }
+    console.log('정상회원이래요');
+    return fulfilledPromiseGen()
+  }).then(data => {
+    console.log('토큰을 저장합니다.');
+  })
 
-  const passwordHandler = (evt) => {
-    setPassword(evt.target.value)
-  }
-
-  const fetchData = (evt) => {
-    evt.preventDefault()
-    const form = evt.target
-    const formData = new FormData(form)
-    const entry = Object.fromEntries(formData)
-    // console.log(entry);
-    const json = JSON.stringify(entry)
-
-    console.log(json)
-
-    // const payload = new URLSearchParams(formData)
-    fetch("http://localhost:4000/login", {
-      method : "POST",
-      headers : {
-        'Content-Type' : 'application/json'
-      },
-      body : json
-    }).then(response => response.json())
-      .then(data => {
-        localStorage.setItem('token', data.accessToken)
-        console.log('받은 토큰은', data.accessToken);
-      })
-      .then(data => console.log(data))
-      .catch(err => console.log('에러발생')) // 여기서 비번 문제인지 없는 유저인지 구분해서 알려줄수있을듯??
-      // .then(data => console.log(data))
-  }
   return (
     <>
-      <h1>로그인하기</h1>
-      <form onSubmit={fetchData}>
-        <input type="text" name = "id" value={id} onChange={idHandler}/>
-        <input type="password" name = "password" value={password} onChange={passwordHandler}/>
-        <button type="submit">생성</button>
-      </form>
+      <h1>실험실</h1>
     </>
   )
 }
