36b114d1fd4ade650ed9c94c3a0d8c5714a46bb8
Yuno Kim <unokim1547@gmail.com>
2025-03-10 02:56:30 +0900

    Test - close Modal by "esc"
    - useEffect cleanup function
    - dependencies

--- insta/src/components/Test/Test.jsx
+++ insta/src/components/Test/Test.jsx
@@ -6,11 +6,45 @@ import {createPortal} from "react-dom"
 import style from './Test.module.css'
 
 const List = ({handler}) => {
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetchData = async () => {
+    await new Promise((res, rej) => {
+      setTimeout(() => {
+        res()
+      }, 2000);
+    })
+
+    setData("데이터가왔어요")
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if(e.key === "Escape") handler()
+    }
+    document.addEventListener("keydown", handleKeyDown)
+    console.log('effect!');
+    setIsLoading(true)
+    fetchData()
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [])
+
+  return (
+    <div className={style.list} >
+      <span>닫기</span>
+      <div>{isLoading ? "로딩중" : "로딩완료"}</div>
+      <div>{data}</div>
+    </div>
+  ) 
+}
+
+const Modal = ({handler}) => {
   return (
     <div className={style["modal-overlay"]}onClick={handler}>
-      <div className={style.list} >
-        <button onClick={handler}>닫기</button>
-      </div>
+      <List handler = {handler}/>
     </div>
   )
 }
@@ -27,7 +61,7 @@ const ModalParent = () => {
     <>
       <span onClick={handler}>팔로워</span>
       {/* { isClick ? createPortal(<button onClick={handler}>모달닫기</button>, document.body) : null } */}
-      { isClick ? createPortal(<List handler = {handler}/>, document.body) : null }
+      { isClick ? createPortal(<Modal handler = {handler}/>, document.body) : null }
     </>
   )
 }
