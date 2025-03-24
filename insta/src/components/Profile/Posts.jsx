06448c3963b2e3d82fedf2440f1e768a427f95c8
Yuno Kim <unokim1547@gmail.com>
2025-03-24 17:31:16 +0900

    Profile post markup action bar

--- insta/src/components/Profile/Posts.jsx
+++ insta/src/components/Profile/Posts.jsx
@@ -4,9 +4,15 @@ import { useContext } from "react"
 import { ModalContext } from "../../context/ModalContext"
 import { PostModalContext } from "../../context/PostModalContext"
 
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
+
 import style from "./Posts.module.css"
 import modalStyle from "../Modal/OverLay.module.css"
 import Skeleton from "../Skeleton/Skeleton"
+import Input from "../Input/Input"
+import Button from "../Button/Button"
+import { Link } from "react-router-dom"
 
 // 이 위치에서 각 post의 id? 에 해당하는 정보를 요청하고 받아야함
 const Article = () => {
@@ -18,7 +24,7 @@ const Article = () => {
   const {isOpen, modalHandler} = useContext(ModalContext)
   const {postID, userID} = useContext(PostModalContext)
   console.log(postID, userID, 'contetext');
-
+  console.log(info);
   const getInfos = async () => {
     // const sleep = await new Promise((res, rej) => {
     //   setTimeout(() => {
@@ -44,7 +50,7 @@ const Article = () => {
 
   // console.log(isOpen, modalHandler);
   return (
-    <div className={modalStyle["modal-overlay"]} onClick={modalHandler}>
+    <div className={modalStyle["modal-overlay"]}>
       <div className={modalStyle["post-modal"]}>
         {isLoading ? "로딩중" : 
         <>
@@ -56,36 +62,48 @@ const Article = () => {
               {/* <div className="hey"> */}
                 <Skeleton type={"image"} width={"40px"} height={"40px"}/>
                 {/* <div className={modalStyle["profile-img"]}>프로필 사진</div> */}
-                <div className={modalStyle.name}>{userID}</div>
+                <Link to = {`/${userID}`} className={modalStyle.name}>{userID}</Link>
               {/* </div> */}
               <div>...</div>
             </div>
-            <div className={`${modalStyle["display-row-container"]} ${modalStyle["post-body"]}`}>
-              <Skeleton type={"image"} width={"40px"} height={"40px"}/>
-              {/* <div className={modalStyle["profile-img"]}>프로필 사진</div> */}
-              <div className={modalStyle.name}>{userID}</div>
-              <div>{info.context}</div>
+            <div className={modalStyle["scroll-view"]}>
+              <div className={`${modalStyle["display-row-container"]} ${modalStyle["post-body"]}`}>
+                <Skeleton type={"image"} width={"40px"} height={"40px"}/>
+                {/* <div className={modalStyle["profile-img"]}>프로필 사진</div> */}
+                <Link to = {`/${userID}`} className={modalStyle.name}>{userID}</Link>
+                {/* <div className={modalStyle.name}>{userID}</div> */}
+                <div>{info.context}</div>
+              </div>
+              <div className={modalStyle["post-comments"]}>
+                {info.comments.map(comment => {
+                return (
+                  <div key={comment.commentID} className={`${modalStyle["display-row-container"]} ${modalStyle["post-comment"]}`}>
+                    <Skeleton type={"image"} width={"40px"} height={"40px"}/>
+                    {/* <div className={modalStyle["profile-img"]}>프로필사진</div> */}
+                    {/* <div className={modalStyle.name}>{comment.user}</div> */}
+                    <Link to = {`/${comment.user}`} className={modalStyle.name}>{comment.user}</Link>
+                    <div>{comment.content}</div>
+                  </div>
+                )
+              })}
+              </div>
+
             </div>
-            <div className={modalStyle["post-comments"]}>
-              {info.comments.map(comment => {
-              return (
-                <div key={comment.commentID} className={`${modalStyle["display-row-container"]} ${modalStyle["post-comment"]}`}>
-                  <Skeleton type={"image"} width={"40px"} height={"40px"}/>
-                  {/* <div className={modalStyle["profile-img"]}>프로필사진</div> */}
-                  <div className={modalStyle.name}>{comment.user}</div>
-                  <div>{comment.content}</div>
-                </div>
-              )
-            })}
+            <div className= {modalStyle["post-actions"]}>
+              <FavoriteBorderOutlinedIcon style = {{fontSize : "30px", padding : "7px", cursor : "pointer", color : "red"}}/>
+              {/* <ModeCommentOutlinedIcon style = {{fontSize : "28px", padding : "7px", cursor : "pointer"}}/> */}
+            {/* <Skeleton type={"image"} width={"40px"} height={"40px"}/> */}
             </div>
-            <div className="post-actions">
-            <Skeleton type={"image"} width={"40px"} height={"40px"}/>
+            <div className={modalStyle["post-meta"]}>
+              <span>{info.likes}</span>
+              <span>{"2025년 3월 24일"}</span>
+            {/* <Skeleton type={"image"} width={"40px"} height={"40px"}/> */}
             </div>
-            <div className="post-meta">
-            <Skeleton type={"image"} width={"40px"} height={"40px"}/>
-            </div>
-            <div className="post-comment-form">
-            <Skeleton type={"image"} width={"40px"} height={"40px"}/>
+            <div className={modalStyle["post-comment-form"]}>
+              {/* <Input placeholder={"댓글 달기..."} style= {{flexGrow : "1", border : "none"}}/> */}
+              <textarea placeholder="댓글 달기"/>
+              <Button text={"게시"} style="blue"/>
+            {/* <Skeleton type={"image"} width={"40px"} height={"40px"}/> */}
             </div>
           </div>
         </>
